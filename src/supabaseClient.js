5463e010db820a1aa6546d9c6ad24cccd290b5c3
Omoding-web <samuel.omoding@student.moringaschool.com>
2026-03-11 17:39:43 +0300

    Reviews

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -6,7 +6,7 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 **Create `.env` in your project root:**
 ```
-VITE_SUPABASE_URL=https://tsiuypvodwlsaeyrbxsq.supabase.co
+VITE_SUPABASE_URL=https://supabase.com/dashboard/org/afpvyriltcxtrgixdqvf
 VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzaXV5cHZvZHdsc2FleXJieHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzAzNzUsImV4cCI6MjA4ODgwNjM3NX0.e7ZbHu6M3Kz2RY6dbHKseEdFxkaQfA-zvM5Gmj1JNhM
 ```
 
