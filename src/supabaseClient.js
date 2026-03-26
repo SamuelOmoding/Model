1d08684e68fbc0ff882940b9cee4461d912bb4b5
Omoding-web <samuel.omoding@student.moringaschool.com>
2026-03-26 16:11:21 +0300

    new

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://tsiuypvodwlsaeyrbxsq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzaXV5cHZvZHdsc2FleXJieHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzAzNzUsImV4cCI6MjA4ODgwNjM3NX0.e7ZbHu6M3Kz2RY6dbHKseEdFxkaQfA-zvM5Gmj1JNhM'
+  'https://wvcquztiubnkzrrjkhhx.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2Y3F1enRpdWJua3pycmpraGh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1Mjg0ODgsImV4cCI6MjA5MDEwNDQ4OH0.dC7NOQXr38AWDTOwlmi9Hs7wD6njtf2s7km2PMh8L-A'
 )
